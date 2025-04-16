e672bce72743d426224cb54b094072021f03fda9
Görkem Çakır <gorkem@logica.blue>
2025-04-16 19:22:40 +0300

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/docs/:path*",
+        destination: "https://trial-app-docs.vercel.app/:path*",
+      },
+    ];
+  },
+};

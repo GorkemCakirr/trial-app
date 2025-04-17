832c9c4af6056fc21b571831d6d3af8bd4e3d0cc
Görkem Çakır <gorkem@logica.blue>
2025-04-17 13:46:13 +0300

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-const DOCS_URL = "https://trial-app-docs.vercel.app/docs";
+const DOCS_URL = "https://trial-app-docs.vercel.app";
 
 export default {
   async rewrites() {
     return [
       {
         source: "/docs",
-        destination: `${DOCS_URL}`,
+        destination: `${DOCS_URL}/docs`,
       },
       {
         source: "/docs/:path*",
-        destination: `${DOCS_URL}/:path*`,
+        destination: `${DOCS_URL}/docs/:path*`,
       },
       {
         source: "/docs-static/:path*",

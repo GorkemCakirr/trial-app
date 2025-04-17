44e6e79a288431e7aea73222c4b13220317d49f1
Görkem Çakır <gorkem@logica.blue>
2025-04-17 14:19:28 +0300

    configuration

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -3,7 +3,7 @@ const rewriteDomain = "https://trial-app-docs.vercel.app";
 
 export async function middleware(request) {
   const {pathname, search} = request.nextUrl;
-  console.log(pathname);
+  console.log("pathname", pathname);
   if (pathname === "/docs") {
     return NextResponse.rewrite(`${rewriteDomain}/docs`);
   }

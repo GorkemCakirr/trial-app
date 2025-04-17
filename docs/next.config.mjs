964bb5bbde9e33d4c07177eab19bd1a21dd6c219
Görkem Çakır <gorkem@logica.blue>
2025-04-17 14:38:30 +0300

    jhjhjkjk

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -4,5 +4,6 @@ const withNextra = nextra({});
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
+  basePath: "/docs",
   assetPrefix: "/docs-static",
 });

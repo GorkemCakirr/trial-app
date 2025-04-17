3e44cf825f0383901a6b9f13139fcbd822e98abd
Görkem Çakır <gorkem@logica.blue>
2025-04-17 12:14:20 +0300

    assetprefixx

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,15 +1,9 @@
 import nextra from "nextra";
 
-const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.jsx",
-});
+const withNextra = nextra({});
 
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   basePath: "/docs",
   assetPrefix: "/docs-static",
-  images: {
-    unoptimized: true,
-  },
 });
